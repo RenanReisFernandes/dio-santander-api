0bdf9352665b1c8295606f39c076a63da4045da3
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-02 14:44:30 -0300

    criation of the Interface UserRepositorie

---
+++ src/main/java/com/santander/dio/projeto/bootcamp/DIO/repositories/UserRepositorie.java
@@ -0,0 +1,7 @@
+package com.santander.dio.projeto.bootcamp.DIO.repositories;
+
+import com.santander.dio.projeto.bootcamp.DIO.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepositorie extends JpaRepository<User,Long> {
+}
