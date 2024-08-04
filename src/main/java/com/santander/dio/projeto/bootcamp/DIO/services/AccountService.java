83a8c2410bfa85fd3ebbbb4d41d482611017d70c
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-04 16:34:42 -0300

    finalization of AccountService

--- src/main/java/com/santander/dio/projeto/bootcamp/DIO/services/AccountService.java
+++ src/main/java/com/santander/dio/projeto/bootcamp/DIO/services/AccountService.java
@@ -5,6 +5,7 @@ import com.santander.dio.projeto.bootcamp.DIO.repositories.AccountRepositorie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,8 +24,35 @@ public class AccountService {
             if(existNumber){
                 throw new RuntimeException("Já existe uma conta criada com o id: "+ existNumber );
             }
-
         }
         return accountRepositorie.save(account);
+    }
+
+    public List<Account> findAll(){
+        return  accountRepositorie.findAll();
+    }
+
+    public Optional<Account> findById(Long id){
+        return accountRepositorie.findById(id);
+    }
+
+    public Account update(Long id, Account updatedAccount){
+        Optional<Account> accountOpt = accountRepositorie.findById(id);
+
+        if(accountOpt.isPresent()){
+            Account accountExistent = accountOpt.get();
+
+            accountExistent.setNumber(updatedAccount.getNumber());
+            accountExistent.setAgency(updatedAccount.getAgency());
+            accountExistent.setBalance(updatedAccount.getBalance());
+            accountExistent.setAccountLimit(updatedAccount.getAccountLimit());
+
+            accountRepositorie.save(accountExistent);
+        }
+        throw new RuntimeException("Conta : "+ id +" não encontrada!");
+    }
+
+    public void delete(Long id){
+        accountRepositorie.findById(id);
     }
 }
