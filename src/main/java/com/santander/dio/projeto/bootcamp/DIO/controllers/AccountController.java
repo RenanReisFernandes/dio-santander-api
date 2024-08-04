3862df976d7951412e77a24345fadee83ab5aac9
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-04 16:41:16 -0300

    criation of method to save Account

--- src/main/java/com/santander/dio/projeto/bootcamp/DIO/controllers/AccountController.java
+++ src/main/java/com/santander/dio/projeto/bootcamp/DIO/controllers/AccountController.java
@@ -1,4 +1,25 @@
 package com.santander.dio.projeto.bootcamp.DIO.controllers;
 
+import com.santander.dio.projeto.bootcamp.DIO.entities.Account;
+import com.santander.dio.projeto.bootcamp.DIO.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
 public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping
+    public ResponseEntity<Account> save(@RequestBody Account account){
+        Account accountSaved = accountService.save(account);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(accountSaved);
+    }
 }
