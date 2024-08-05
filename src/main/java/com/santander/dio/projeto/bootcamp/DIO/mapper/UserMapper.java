199146f1886a8168aaed2f52dec39f4935c4ec92
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-05 14:14:23 -0300

    criation of method toUser in Class Mapper for DTO

--- src/main/java/com/santander/dio/projeto/bootcamp/DIO/mapper/UserMapper.java
+++ src/main/java/com/santander/dio/projeto/bootcamp/DIO/mapper/UserMapper.java
@@ -1,4 +1,17 @@
 package com.santander.dio.projeto.bootcamp.DIO.mapper;
 
+import com.santander.dio.projeto.bootcamp.DIO.DTO.request.UserRequest;
+import com.santander.dio.projeto.bootcamp.DIO.entities.User;
+
 public class UserMapper {
+
+    public static User toUser(UserRequest userRequest){
+        User user = new User();
+        user.setName(userRequest.getName());
+        user.setCpf(userRequest.getCpf());
+        user.setPhoneNumber(userRequest.getPhoneNumber());
+        user.setBirthDate(userRequest.getBirthDate());
+        user.setAddress(userRequest.getAddress());
+        return user;
+    }
 }
