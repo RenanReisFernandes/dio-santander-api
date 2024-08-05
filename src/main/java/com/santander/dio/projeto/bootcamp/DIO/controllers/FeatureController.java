98f24b947ee88413725df54c5c0b02f79b8e2614
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-04 23:53:38 -0300

    criation of endpoint to create fature in FeatureController class

--- src/main/java/com/santander/dio/projeto/bootcamp/DIO/controllers/FeatureController.java
+++ src/main/java/com/santander/dio/projeto/bootcamp/DIO/controllers/FeatureController.java
@@ -1,9 +1,24 @@
 package com.santander.dio.projeto.bootcamp.DIO.controllers;
 
+import com.santander.dio.projeto.bootcamp.DIO.entities.Feature;
+import com.santander.dio.projeto.bootcamp.DIO.services.FeatureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/feature")
 public class FeatureController {
+    @Autowired
+    private FeatureService featureService;
+
+    @PostMapping
+    public ResponseEntity<Feature> save(@RequestBody Feature feature){
+        Feature featureSaved = featureService.save(feature);
+        return ResponseEntity.status(HttpStatus.CREATED).body(featureSaved);
+    }
 }
