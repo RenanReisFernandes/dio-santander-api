e93d3857d129f63d39688a111a947c99466f1469
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-04 22:51:33 -0300

    criation of ControllerCard

---
+++ src/main/java/com/santander/dio/projeto/bootcamp/DIO/controllers/CardController.java
@@ -0,0 +1,9 @@
+package com.santander.dio.projeto.bootcamp.DIO.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/card")
+public class CardController {
+}
