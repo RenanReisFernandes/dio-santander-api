633fbe2901ccd05a874dbad473adc34611d340fa
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-04 23:35:43 -0300

    criation of method findById in FeatureService

--- src/main/java/com/santander/dio/projeto/bootcamp/DIO/services/FeatureService.java
+++ src/main/java/com/santander/dio/projeto/bootcamp/DIO/services/FeatureService.java
@@ -5,6 +5,10 @@ import com.santander.dio.projeto.bootcamp.DIO.repositories.FeatureRepositorie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class FeatureService {
     @Autowired
@@ -13,5 +17,12 @@ public class FeatureService {
     public Feature save(Feature feature){
         return featureRepositorie.save(feature);
     }
-    
+
+    public List<Feature> findAll(){
+        return featureRepositorie.findAll();
+    }
+
+    public Optional<Feature> findById(Long id){
+       return featureRepositorie.findById(id);
+    }
 }
