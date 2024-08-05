6d195f7313d0101b7e2fa2c4407799c32b5d9e93
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-05 13:05:49 -0300

    criation of attributes of UserResponse

--- src/main/java/com/santander/dio/projeto/bootcamp/DIO/DTO/response/UserResponse.java
+++ src/main/java/com/santander/dio/projeto/bootcamp/DIO/DTO/response/UserResponse.java
@@ -1,4 +1,32 @@
 package com.santander.dio.projeto.bootcamp.DIO.DTO.response;
 
+import com.santander.dio.projeto.bootcamp.DIO.entities.Account;
+import com.santander.dio.projeto.bootcamp.DIO.entities.Card;
+import com.santander.dio.projeto.bootcamp.DIO.entities.Feature;
+import com.santander.dio.projeto.bootcamp.DIO.entities.News;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class UserResponse {
+
+    private String name;
+    @OneToMany
+    private Set<Account> accounts;
+    @OneToOne
+    private Card card;
+    @OneToMany
+    private List<Feature> features;
+    @OneToMany
+    private List<News> news;
 }
