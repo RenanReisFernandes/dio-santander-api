f783a2ebee74b06837b940f85d6dbda6dbcee892
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-04 23:32:03 -0300

    criation of method save in FeatureService

--- src/main/java/com/santander/dio/projeto/bootcamp/DIO/services/FeatureService.java
+++ src/main/java/com/santander/dio/projeto/bootcamp/DIO/services/FeatureService.java
@@ -1,7 +1,17 @@
 package com.santander.dio.projeto.bootcamp.DIO.services;
 
+import com.santander.dio.projeto.bootcamp.DIO.entities.Feature;
+import com.santander.dio.projeto.bootcamp.DIO.repositories.FeatureRepositorie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FeatureService {
+    @Autowired
+    private FeatureRepositorie featureRepositorie;
+
+    public Feature save(Feature feature){
+        return featureRepositorie.save(feature);
+    }
+    
 }
