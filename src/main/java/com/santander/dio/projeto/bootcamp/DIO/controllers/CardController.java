88cf37fe33077873a919cc349dc0e41fe692c8bb
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-04 22:54:22 -0300

    criation of method save to ControllerCard

--- src/main/java/com/santander/dio/projeto/bootcamp/DIO/controllers/CardController.java
+++ src/main/java/com/santander/dio/projeto/bootcamp/DIO/controllers/CardController.java
@@ -1,9 +1,25 @@
 package com.santander.dio.projeto.bootcamp.DIO.controllers;
 
+import com.santander.dio.projeto.bootcamp.DIO.entities.Card;
+import com.santander.dio.projeto.bootcamp.DIO.services.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/card")
 public class CardController {
+    @Autowired
+    private CardService cardService;
+
+    @PostMapping
+    public ResponseEntity<Card> save(@RequestBody Card card){
+        Card cardCreated = cardService.save(card);
+        return ResponseEntity.status(HttpStatus.CREATED).body(card);
+    }
 }
